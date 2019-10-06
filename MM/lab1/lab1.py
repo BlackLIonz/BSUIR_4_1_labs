765be49cce4416258acd4205c377f82564aed427
BlackLionz <shotaik@mail.ru>
2019-10-06 22:39:13 +0300

    'Histograms'

--- MM/lab1/lab1.py
+++ MM/lab1/lab1.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def multiplicative_congruent_method(a=214013, m=2**32):
     f = open('A', 'r')
     A = int(f.readline())
@@ -15,11 +19,24 @@ def check_equability(k, n):
     start = 0
     dct = {}
     for i in range(k):
-        dct[(round(start, 6), round(start + interval, 6))] = len([number for number in numbers if start < number < start + interval]) / n
+        dct[(round(start, 6), round(start + interval, 6))] = len([number for number in numbers if start <= number < start + interval]) / n
         start += interval
-    return dct
+    return dct, numbers
+
+
+def print_histogram_equability(k, n):
+    equ, numbers = check_equability(k, n)
+    keys = list(equ.keys())
+    x = [key[0] for key in keys]
+    y = list(equ.values())
+    width = keys[0][1] - keys[0][0]
+    plt.bar(x, y, width)
+    m = sum(numbers) / n
+    S = ((1 / (n - 1)) * sum([(z - m) ** 2 for z in numbers]))
+    print(f'{m} -> 1/2 = 0.5')
+    print(f'{S} -> 1/12 = 0.083333')
+    plt.show()
 
 
 if __name__ == '__main__':
-    res = check_equability(10, 100)
-    print(res)
+    print_histogram_equability(10, 1000)
