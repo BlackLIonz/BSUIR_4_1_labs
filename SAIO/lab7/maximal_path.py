b3c7a932b9537ee7b165a331340e3e085282fcf3
BlackLionz <shotaik@mail.ru>
2019-10-31 20:20:42 +0300

    Start lab7

--- SAIO/lab7/maximal_path.py
+++ SAIO/lab7/maximal_path.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class DijkstraAlgorithm:
+class MaximalPath:
     def __init__(self, matrix, weights, s, t):
         self.matrix = matrix
         self.weights = weights
@@ -9,34 +9,43 @@ class DijkstraAlgorithm:
         self.t = t
         self.f = {}
         self.B = dict(zip(range(1, len(matrix) + 1), [np.inf] * len(matrix)))
+        self.I = []
 
         self.B[s] = 0
         self.f[s] = 0
 
+    def w(self, I):
+        non_visited_neighbors = []
+        for i in I:
+            for j in self.matrix[i]:
+                if j not in self.I:
+                    non_visited_neighbors.append(j)
+        return non_visited_neighbors
+
+    def I_minus(self, I):
+        neighbors = []
+        for i in I:
+            pass
+
+        return neighbors
+
     def solve(self):
-        visited = list(range(1, len(self.matrix) + 1))
-        pointer = self.s
-        while len(visited):
-            neighbors = self.matrix[pointer]
-            for neighbor in neighbors:
-                neighbor_weight = self.B[pointer] + self.weights[(pointer, neighbor)]
-                if self.B[neighbor] > neighbor_weight:
-                    self.B[neighbor] = neighbor_weight
-                    self.f[neighbor] = pointer
-            visited.remove(pointer)
-            pointer = min(self.B.items(), key=lambda x: x[1] if x[0] in visited else np.inf)[0]
-        return self.f
+        self.I.append(self.s)
+        self.B[self.s] = 0
+        self.f[self.s] = 0
+        while True:
+            w_I = self.w(self.I)
+
 
 
 if __name__ == '__main__':
     matrix = {
         1: [2, 6],
-        2: [3],
-        3: [4, 5],
+        2: [3, 5],
+        3: [4],
         4: [],
-        5: [7],
-        6: [2, 5, 7],
-        7: [2, 3],
+        5: [3, 4],
+        6: [2, 3, 5],
     }
     weights = {
         (1, 2): 12,
@@ -51,7 +60,7 @@ if __name__ == '__main__':
         (7, 2): 2,
         (7, 3): 6,
     }
-    dijkstra = DijkstraAlgorithm(matrix, weights, 1, 4)
+    dijkstra = MaximalPath(matrix, weights, 1, 4)
     path = dijkstra.solve()
     pointer = 4
     res = []
