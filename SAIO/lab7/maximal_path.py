2f6de1d5bce295ad107ac3a6dede85c54cfcdf58
BlackLionz <shotaik@mail.ru>
2019-10-31 23:53:29 +0300

    End lab7

--- SAIO/lab7/maximal_path.py
+++ SAIO/lab7/maximal_path.py
@@ -15,27 +15,52 @@ class MaximalPath:
         self.f[s] = 0
 
     def w(self, I):
-        non_visited_neighbors = []
+        non_visited_neighbors = set()
         for i in I:
             for j in self.matrix[i]:
                 if j not in self.I:
-                    non_visited_neighbors.append(j)
+                    non_visited_neighbors.add(j)
         return non_visited_neighbors
 
-    def I_minus(self, I):
+    def I_minus(self, i):
         neighbors = []
-        for i in I:
-            pass
-
+        for key, value in self.matrix.items():
+            if i in value:
+                neighbors.append(key)
         return neighbors
+
+    def find_subset(self, I):
+        for i in I:
+            I_minus = self.I_minus(i)
+            if self.if_subset(self.I, I_minus):
+                return i
+
+    @staticmethod
+    def if_subset(lst, sub_lst):
+        return set(sub_lst).issubset(set(lst))
 
     def solve(self):
         self.I.append(self.s)
         self.B[self.s] = 0
         self.f[self.s] = 0
-        while True:
+        while len(self.f) != len(self.matrix):
             w_I = self.w(self.I)
+            j_astr = self.find_subset(w_I)
+            B_i, i = self.get_B(j_astr)
+            self.B[j_astr] = B_i
+            self.f[j_astr] = i
+            self.I.append(j_astr)
+        return self.f
 
+    def get_B(self, j_astr):
+        maximum = -np.inf
+        max_i = None
+        for i in self.I_minus(j_astr):
+            new_B = self.B[i] + self.weights[(i, j_astr)]
+            if new_B > maximum:
+                maximum = new_B
+                max_i = i
+        return maximum, max_i
 
 
 if __name__ == '__main__':
@@ -48,20 +73,19 @@ if __name__ == '__main__':
         6: [2, 3, 5],
     }
     weights = {
-        (1, 2): 12,
+        (1, 2): 2,
         (1, 6): 1,
         (2, 3): 2,
+        (2, 5): 7,
         (3, 4): 1,
-        (3, 5): 5,
-        (5, 7): 2,
-        (6, 2): 10,
-        (6, 5): 5,
-        (6, 7): 8,
-        (7, 2): 2,
-        (7, 3): 6,
+        (5, 3): 1,
+        (5, 4): 1,
+        (6, 2): 4,
+        (6, 3): 4,
+        (6, 5): 1,
     }
-    dijkstra = MaximalPath(matrix, weights, 1, 4)
-    path = dijkstra.solve()
+    max_path = MaximalPath(matrix, weights, 1, 4)
+    path = max_path.solve()
     pointer = 4
     res = []
     while len(res) != len(path):
