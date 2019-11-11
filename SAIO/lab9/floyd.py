876150dcc946a02cceb1e854a990472524d11670
BlackLionz <shotaik@mail.ru>
2019-11-11 21:08:41 +0300

    End lab9

--- SAIO/lab9/floyd.py
+++ SAIO/lab9/floyd.py
@@ -1,23 +1,42 @@
+import copy
+
 import numpy as np
 
 
 class Floyd:
-    def __init__(self, D):
-        self.D = D
-        self.length = len(self.D)
-        self.R = [list(range(1, self.length + 1))] * self.length
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.length = len(self.matrix)
+        self.R = np.array([list(range(self.length))] * self.length)
+        self.D = np.array([])
 
-    def solve(self):
+    def solve(self, start, end):
+        start -= 1
+        end -= 1
+        if len(self.D) == 0:
+            self.D = copy.deepcopy(self.matrix)
+            self.calculate_paths()
+        path = [start]
+        now = start
+        while now != end:
+            now = self.R[now, end]
+            path.append(now)
+        print(' -> '.join(map(lambda x: str(x + 1), path)))
+
+    def calculate_paths(self):
         for i in range(len(self.D)):
             indexes = self.get_working_matrix(i)
-            d = self.get_d(indexes, i)
+            self.get_d(indexes, i)
 
-    def get_d(self, matrix, k):
-        d = {}
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                if i == j:
+    def get_d(self, matrix, j):
+        for i in matrix:
+            for k in matrix:
+                if i == j or k == j or i == k:
                     continue
+                d = self.D[i, j] + self.D[j, k]
+                if self.D[i, k] > d:
+                    self.D[i, k] = d
+                    self.R[i, k] = j
 
     def get_working_matrix(self, i):
         indexes = []
@@ -29,13 +48,12 @@ class Floyd:
 
 if __name__ == '__main__':
     D = np.array([[0, 9, np.inf, 3, np.inf, np.inf, np.inf, np.inf],
-         [9, 0, 2, np.inf, 7, np.inf, np.inf, np.inf],
-         [np.inf, 2, 0, 2, 4, 8, 6, np.inf],
-         [3, np.inf, 2, 0, np.inf, np.inf, 5, np.inf],
-         [np.inf, 7, 4, np.inf, 0, 10, np.inf, np.inf],
-         [np.inf, np.inf, 8, np.inf, 10, 0, 7, np.inf],
-         [np.inf, np.inf, 6, 5, np.inf, 7, 0, np.inf],
-         [np.inf, np.inf, np.inf, np.inf, 9, 12, 10, 0]])
+                  [9, 0, 2, np.inf, 7, np.inf, np.inf, np.inf],
+                  [np.inf, 2, 0, 2, 4, 8, 6, np.inf],
+                  [3, np.inf, 2, 0, np.inf, np.inf, 5, np.inf],
+                  [np.inf, 7, 4, np.inf, 0, 10, np.inf, np.inf],
+                  [np.inf, np.inf, 8, np.inf, 10, 0, 7, np.inf],
+                  [np.inf, np.inf, 6, 5, np.inf, 7, 0, np.inf],
+                  [np.inf, np.inf, np.inf, np.inf, 9, 12, 10, 0]])
     floyd = Floyd(D)
-    floyd.solve()
-
+    floyd.solve(1, 5)
